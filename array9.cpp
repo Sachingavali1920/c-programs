9e6cb6b5b5cdf1cf624891e3c39f968f457e1297
Sachingavali1920 <98200003+Sachingavali1920@users.noreply.github.com>
2022-01-25 01:23:54 +0530

    array besic progrms

---
+++ array9.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num [4]={4,5,6,7};
+    cout << "THE NUMBER IS ";
+    
+    for(int i =0; i<=4; i++)
+    cout<<"\n the number is "<<" \n "<< num[i] ;
+    return 0;
+
+}
